75c6b37f1380134a1ea3e5a2ef0d0adcc915de45
ViktorKornelis <viktor.kornelis@student.hogent.be>
2023-03-13 20:05:13 +0100

    initial commit

---
+++ Installatie.ps1
@@ -0,0 +1,14 @@
+﻿# Automatiseren software-installatie
+Write-Host "Installatie algemene applicaties" 
+choco install -y git
+choco install -y adobereader
+choco install -y firefox
+choco install -y github
+choco install -y vscode
+choco install -y vlc
+Write-Host "Software voor System Engineering Lab"
+choco install -y filezilla
+choco install -y virtualbox
+choco install -y mysql.workbench
+Write-Host "updaten tot laatste versie"
+choco upgrade all -y
